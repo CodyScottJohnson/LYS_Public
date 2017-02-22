f74fd7e83469e3a1340bcd1f68e139b105186d30
Cody Johnson <cody@anpac.net>
2017-02-22 08:35:46 -0700

    maybe

---
+++ app/scripts/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name LYS_Public
+ * @description
+ * # LYS_Public
+ *
+ * Main module of the application.
+ */
+angular
+  .module('LYS_Public', [
+    'angular-carousel-3d',
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ui.router',
+    'ngMaterial'
+  ])
+  .run(function() {
+
+  })
+  .config(function($stateProvider, $urlRouterProvider,$locationProvider) {
+    $urlRouterProvider.otherwise("/");
+    //$locationProvider.html5Mode(true);
+    $stateProvider
+      .state('app', {
+        url: '',
+        abstract: true,
+        templateUrl: 'views/app.html',
+        controller: 'MainCtrl',
+        data: {
+          requireLogin: true
+        }
+      })
+      .state('app.Main', {
+        url: '/',
+        templateUrl: 'views/General/landing.html',
+        controller: 'MainCtrl',
+
+      })
+
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lysAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lysAdminApp
+ */
+angular.module('LYS_Public')
+  .controller('MainCtrl', function ($scope) {
+    $scope.temp = [0,1,2,3,4];
+  });
